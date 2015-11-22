9f0ef4ae437bd53094b1e6c090fc8df4b93f5d66
Shadow8t4 <adh9694@gmail.com>
2015-11-22 09:44:23 -0600

    It play. I fix.

--- gamerun.js
+++ gamerun.js
@@ -56,166 +56,156 @@ var tile =
 ['.','.','.','#','.','.','.','.','.','.','.','#','.','.','.','.','.','.','.','#','.','.','.','.','.','.','.','#','.','.','.','.']
 ];
 
-player = {x: 0, y: 0};
-player2 = {x: (tile.length - 1), y: (tile.length - 1)};
-offx = 0;
-offy = 0;
-dirx = 0;
-diry = 0;
-dirpx = 0;
-dirpy = 0;
+var player = {x: 0, y: 0};
+var player2 = {x: (tile.length - 1), y: (tile.length - 1)};
+var offx = 0;
+var offy = 0;
+var gb;
+
+var server = new TwoSocket("ws://localhost:8080");
 
 
-tempx = 0;
-tempy = 0;
-
-
-function drawGhost(x, y) {
-	var alpha = 1;
-	var s = setInterval(function(){
-		if((tempx !== player.x) || (tempy !== player.y)) {
-			x += (tempx - player.x)*128;
-			y += (tempy - player.y)*128;
-			tempx = player.x;
-			tempy = player.y;
-		}
-		cte.clearRect((canvas.width/2 + x - 64),(canvas.height/2 + y - 64),128,128);
-		cte.fillStyle = "rgba(0,0,0,"+alpha+")";
-		cte.fillRect((canvas.width/2 + x - 64),(canvas.height/2 + y - 64),128,128);
-		alpha-=0.025;
-		if(alpha <= 0) {
-			cte.clearRect((canvas.width/2 + x - 64),(canvas.height/2 + y - 64),128,128);
-			clearInterval(s);
-		}
-	}, 10);
-}
-
-var s = setInterval(function() {
-	
-				
-	//0, 103, 219
-	ctx.fillStyle = "#0067db";
-	/*
-    for(r=0; r < canvas.width; r++) 
-    {
-        ctx.drawImage(bg, (bg.width/50)*r, 0, bg.width, 1024);
-    }
-	*/
-	ctx.fillRect(0,0,canvas.width,canvas.height);
-
-	for(i = 0; i < tile.length; i++)
-	{
-		for(j = 0; j < tile[i].length; j++)
-		{
-			if(tile[i][j] == '.') {
-				//ctx.strokeStyle = "#000000";
-				ctx.drawImage(pathTile, (((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
-			}
-			if(tile[i][j] == '#') {
-				//ctx.strokeStyle = "#0000ff";
-				ctx.drawImage(wallTile, (((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
-			}
-			if(tile[i][j] == '$') {
-				//ctx.fillStyle = "#8585dd";
-				//ctx.fillRect((((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
-				//ctx.strokeStyle = "#ffffff";
-				//ctx.strokeRect((((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
-				ctx.drawImage(finTile, (((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
-			}
-
-						// Center the player 	Player size   Tile offset	32 is for center offset
-			//ctx.strokeRect((((canvas.width/2) - player.x*64 + j*64)- 32), ((canvas.height/2) - player.y*64 + i*64 - 32), 64, 64);
-		}
-	}
-	
-
-	ctx.fillStyle = "#ffffff";
-
-			// Center the player 	32 is for center offset
-	ctx.drawImage(playerPic, ((canvas.width/2) - 64), ((canvas.height/2) - 64), 128, 128);
-
-			// Center the player 	Player2 size   Player offset	64 is for center offset
-	ctx.drawImage(player2Pic, ((canvas.width/2) + player2.x*128 - (player.x*128) - 64), ((canvas.height/2) + player2.y*128 - (player.y*128) - 64), 128, 128);
-
-}, 10);
-
-
-//Cool game design idea: since we can't interpolate the movements of the other player, have them leave a fading trail and align them to the coordinates
-
-function moveLeft(player) {
-	if((player.x > 0) && (tile[player.y][player.x - 1] != '#'))
-	{
-		//drawGhost((0), (0));
-		player.x--;
-	}
-}
-
-function moveRight(player) {
-	if((player.x < (tile.length - 1)) && (tile[player.y][player.x + 1] != '#'))
-	{
-		//drawGhost((0), (0));
-		player.x++;
-	}
-}
-
-function moveDown(player) {
-	if((player.y > 0) && (tile[player.y - 1][player.x] != '#'))
-	{
-		//drawGhost((0), (0));
-		player.y--;
-	}
-}
-
-function moveUp(player) {
-	if((player.y < (tile.length - 1)) && (tile[player.y + 1][player.x] != '#'))
-	{
-		//drawGhost((0), (0));
-		player.y++;
-	}
-}
-
-function gameOver() {
-	if((tile[player.x][player.y] == '$') && (tile[player2.x][player2.y] == '$')) {
-		
-	}
-}
-
-canvas.addEventListener("keydown", function(e){
-	
-	//player
-
-	if(e.keyCode == 37) {
-		moveLeft(player);
-	}
-	if(e.keyCode == 39) {
-		moveRight(player);
-	}
-	if(e.keyCode == 38) {
-		moveDown(player);
-	}
-	if(e.keyCode == 40) {
-		moveUp(player);
-	}
-	
-	
-	//player 2
-
-	if(e.keyCode == 65) {
-		moveLeft(player2);
-	}
-	if(e.keyCode == 68) {
-		moveRight(player2);
-	}
-	if(e.keyCode == 87) {
-		moveDown(player2);
-	}
-	if(e.keyCode == 83) {
-		moveUp(player2);
-	}
-
-
-
+document.addEventListener("game_created", function(e) {
+	alert("Awating connection.");
 });
+
+document.addEventListener("game_ready", function(e) {
+	gb = new Gameboard();
+});
+
+document.addEventListener('disconnect', function(e) {
+    console.log(e.detail.message);
+    gb.stop();
+});
+
+server.send("game.mode", {mode:0});
+
+function Gameboard()
+{
+	var gb = this;
+	var s = setInterval(function() {
+		
+		ctx.fillStyle = "#0067db";
+		ctx.fillRect(0,0,canvas.width,canvas.height);
+
+		for(i = 0; i < tile.length; i++)
+		{
+			for(j = 0; j < tile[i].length; j++)
+			{
+				if(tile[i][j] == '.') {
+					ctx.drawImage(pathTile, (((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
+				}
+				if(tile[i][j] == '#') {
+					ctx.drawImage(wallTile, (((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
+				}
+				if(tile[i][j] == '$') {
+					ctx.drawImage(finTile, (((canvas.width/2) - player.x*128 + j*128)- 64), ((canvas.height/2) - player.y*128 + i*128 - 64), 128, 128);
+				}
+			}
+		}
+
+				// Center the player 	64 is for center offset
+		ctx.drawImage(playerPic, ((canvas.width/2) - 64), ((canvas.height/2) - 64), 128, 128);
+
+				// Center the player 	Player2 size   Player offset				64 is for center offset
+		ctx.drawImage(player2Pic, ((canvas.width/2) + player2.x*128 - (player.x*128) - 64), ((canvas.height/2) + player2.y*128 - (player.y*128) - 64), 128, 128);
+
+	}, 10);
+
+
+	function moveLeft(player) {
+		if((player.x > 0) && (tile[player.y][player.x - 1] != '#'))
+		{
+			player.x--;
+			server.send("game.coordinates", player);
+			gameOver();
+		}
+	}
+
+	function moveRight(player) {
+		if((player.x < (tile.length - 1)) && (tile[player.y][player.x + 1] != '#'))
+		{
+			player.x++;
+			server.send("game.coordinates", player);
+			gameOver();
+		}
+	}
+
+	function moveDown(player) {
+		if((player.y > 0) && (tile[player.y - 1][player.x] != '#'))
+		{
+			player.y++;
+			server.send("game.coordinates", player);
+			gameOver();
+		}
+	}
+
+	function moveUp(player) {
+		if((player.y < (tile.length - 1)) && (tile[player.y + 1][player.x] != '#'))
+		{
+			player.y--;
+			server.send("game.coordinates", player);
+			gameOver();
+		}
+	}
+
+	function gameOver() {
+		if((tile[player.x][player.y] == '$') && (tile[player2.x][player2.y] == '$')) {
+			console.log("game over.");
+			server.send("game.stop", {});
+			gb.stop();
+		}
+		console.log(tile[player.x][player.y] + " " + tile[player2.x][player2.y]);
+	}
+
+	document.addEventListener('coordinates', function(e) {
+		//console.log(e.detail.message);
+		player2.x = e.detail.message.x;
+		player2.y = e.detail.message.y;
+		gameOver();
+	});
+
+	canvas.addEventListener("keydown", function(e){
+		
+		//player
+
+		if(e.keyCode == 37) {
+			moveLeft(player);
+		}
+		if(e.keyCode == 39) {
+			moveRight(player);
+		}
+		if(e.keyCode == 38) {
+			moveDown(player);
+		}
+		if(e.keyCode == 40) {
+			moveUp(player);
+		}
+		
+		
+		/*player 2
+
+		if(e.keyCode == 65) {
+			moveLeft(player2);
+		}
+		if(e.keyCode == 68) {
+			moveRight(player2);
+		}
+		if(e.keyCode == 87) {
+			moveDown(player2);
+		}
+		if(e.keyCode == 83) {
+			moveUp(player2);
+		}
+		*/
+
+
+	});
+
+	this.stop = function() {
+		clearInterval(s);
+	}
+}
 
 //left = 37, right = 39, up = 38, down = 40
 
---
+++ tosocket.js
@@ -0,0 +1,87 @@
+function TwoSocket(url) {
+  var socket = new WebSocket(url);
+  var waiting = true;
+  var failed = false;
+  var queue = [];
+  var defaultPayload = {};
+
+  var count = 0;
+  var failure = setInterval(function() {
+    if (count == 5000/50) {
+      console.log("Connection not established");
+      failed = true;
+      clearInterval(failure);
+    }
+    count++;
+  }, 50);
+
+  socket.onmessage = function (event) {
+    clearInterval(failure);
+    jdata = JSON.parse(event.data);
+    if (jdata['status'] == 'connected') {
+      waiting = false;
+      defaultPayload['id'] = jdata['id'];
+    } else {
+      failed = true;
+      waiting = false;
+      console.log("Error: Response not expected:");
+      console.log(event.data);
+    }
+  }
+
+  var queueInterval = setInterval(function() {
+    if (!waiting) {
+      if (queue.length > 0) {
+        send = queue.shift();
+        send();
+      }
+    }
+
+    if (failed) {
+      clearInterval(queueInterval);
+    }
+  }, 25);
+
+  this.send = function(path, data) {
+    if (failed) {
+      console.log("Connection not established");
+      return;
+    }
+
+    queue.push(function() {
+      waiting = true;
+
+      payload = {
+        path: path,
+        data: data,
+        id: defaultPayload['id']
+      };
+
+      socket.send(JSON.stringify(payload));
+
+      var count = 0;
+      var timeout = setInterval(function() {
+        if (count == 1000/50) {
+          console.log("Couldn't reach host");
+          waiting = false;
+          clearInterval(timeout);
+        }
+        count++;
+      }, 50);
+
+      socket.onmessage = function (event) {
+        clearInterval(timeout);
+        jdata = JSON.parse(event.data);
+        var event;
+        if (jdata['error']) {
+          jdata['error'];
+          event = new CustomEvent('error', { detail: jdata['error'] })
+        } else {
+          event = new CustomEvent(jdata['name'], { detail: jdata['args'] });
+        }
+        document.dispatchEvent(event);
+        waiting = false;
+      };
+    });
+  };
+}
